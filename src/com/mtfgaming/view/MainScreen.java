c9715bf19dc12fa0e669b347add536ed0a2a32a8
Netsky <Netsky@Netsky-BPC>
2015-09-27 17:53:19 +0200

    added functionality to Character and Character Type, new tab: Encounters

--- src/com/mtfgaming/view/MainScreen.java
+++ src/com/mtfgaming/view/MainScreen.java
@@ -3,6 +3,8 @@
  */
 package com.mtfgaming.view;
 
+import com.mtfgaming.model.Character;
+import com.mtfgaming.model.CharacterType;
 import com.mtfgaming.model.GameList;
 import com.mtfgaming.model.GameType;
 import com.mtfgaming.model.LookUpTable;
@@ -12,10 +14,16 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
@@ -24,6 +32,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.GridPane;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
 
@@ -35,6 +44,8 @@ public class MainScreen extends ListView implements Initializable {
     
     @FXML private ListView<String> gameTypeList;
     @FXML private ListView<String> tableList;
+    @FXML private ListView<String> charTypeList;
+    @FXML private ListView<String> charList;
     @FXML private TableView<Map> lookupTable;
     @FXML private TableColumn<Map, String> tableName;
     @FXML private TableColumn<Map, String> tableDesc;
@@ -47,6 +58,8 @@ public class MainScreen extends ListView implements Initializable {
     private String selectedGT;
     private String selectedTable;
     private String editedTableEntry;
+    private String selectedCT;
+    private String selectedChar;
     
 
     @Override
@@ -54,6 +67,10 @@ public class MainScreen extends ListView implements Initializable {
         tableName.setCellValueFactory(new MapValueFactory(LookUpTable.TABLE_KEY));
         tableDesc.setCellValueFactory(new MapValueFactory(LookUpTable.TABLE_VALUE));
         this.reloadGameList();
+        if(!gl.getObsList().isEmpty()) {
+            gameTypeList.getSelectionModel().selectFirst();
+            handleGTSelection();
+        }
     }
     
     private void reloadGameList() {
@@ -63,7 +80,7 @@ public class MainScreen extends ListView implements Initializable {
     @FXML 
     public void handleGTSelection() {
         String item = gameTypeList.getSelectionModel().getSelectedItem();
-        if(item != null && !item.equals(selectedGT)) {
+        if(item != null) {
             this.selectedGT = item;
             this.loadGT();
         }
@@ -81,6 +98,8 @@ public class MainScreen extends ListView implements Initializable {
             gt.setName(result.get());
             gl.addGame(gt);
             this.reloadGameList();
+            gameTypeList.getSelectionModel().select(gt.getName());
+            this.handleGTSelection();
         }
     }
     
@@ -114,13 +133,20 @@ public class MainScreen extends ListView implements Initializable {
             Optional<ButtonType> result = alert.showAndWait();
             if (result.get() == ButtonType.OK){
                 gl.removeGame(gl.getGame(item));
+                this.reloadGameList();
+                gameTypeList.getSelectionModel().selectFirst();
+                handleGTSelection();
             }
         }
     }
     
     private void loadGT() {
-        tableList.setItems(gl.getGame(selectedGT).getObsList());
-        this.tableAddEntryButton.setDisable(true);
+        if (getSelectedGame() != null) {
+            tableList.setItems(this.getSelectedGame().getTableObsList());
+            this.tableAddEntryButton.setDisable(true);
+            charTypeList.setItems(this.getSelectedGame().getCharTypeObsList());
+            charList.setItems(this.getSelectedGame().getCharObsList());
+        }
     }
     
     @FXML 
@@ -136,12 +162,12 @@ public class MainScreen extends ListView implements Initializable {
     @FXML 
     public void handleTableAddEntry() {
         if(!tableAddEntryKey.getText().isEmpty() && !tableAddEntryKey.getText().trim().equals("")) {
-            gl.getGame(selectedGT).getTable(selectedTable)
+            this.getSelectedGame().getTable(selectedTable)
                     .addEntry(tableAddEntryKey.getText(), tableAddEntryValue.getText());
             tableAddEntryKey.setText("");
             tableAddEntryValue.setText("");
             this.reloadTable();
-            gl.saveGame(selectedGT);
+            gl.saveGame(getSelectedGame());
         }
     }
     
@@ -155,20 +181,20 @@ public class MainScreen extends ListView implements Initializable {
         String name = (String) lookupTable.getSelectionModel().getSelectedItem().get(LookUpTable.TABLE_KEY);
         String desc = (String) lookupTable.getSelectionModel().getSelectedItem().get(LookUpTable.TABLE_VALUE);
         System.out.println(name + " " + desc);
-        gl.getGame(selectedGT).getTable(selectedTable).removeEntry(editedTableEntry);
+        this.getSelectedGame().getTable(selectedTable).removeEntry(editedTableEntry);
         if(!name.isEmpty()) {
-            gl.getGame(selectedGT).getTable(selectedTable).addEntry(name, desc);
+            this.getSelectedGame().getTable(selectedTable).addEntry(name, desc);
         }
         this.reloadTable();
-        gl.saveGame(selectedGT);
+        gl.saveGame(getSelectedGame());
     }
     
     private void loadTable() {
-        Callback<TableColumn<Map, String>, TableCell<Map, String>>
-            cellFactoryForMap = new Callback<TableColumn<Map, String>,TableCell<Map, String>>() {
-                @Override
-                public TableCell call(TableColumn p) {
-                    return new TextFieldTableCell(new StringConverter() {
+        Callback<TableColumn<Map, String>, TableCell<Map, String>> cellFactoryForMap;
+        cellFactoryForMap = new Callback<TableColumn<Map, String>,TableCell<Map, String>>() {
+            @Override
+            public TableCell call(TableColumn p) {
+                return new TextFieldTableCell(new StringConverter() {
                     @Override
                     public String toString(Object t) {
                         return t.toString();
@@ -186,39 +212,198 @@ public class MainScreen extends ListView implements Initializable {
     }
     
     private void reloadTable() {
-        lookupTable.setItems(gl.getGame(selectedGT).getTable(selectedTable).getObsList());
+        lookupTable.setItems(this.getSelectedGame().getTable(selectedTable).getObsList());
     }
     
     @FXML 
     public void handleTableAdd() {
-        TextInputDialog dialog = new TextInputDialog("New Table");
-        dialog.setTitle("New Table");
-        dialog.setHeaderText("Create a new Lookup Table");
-        dialog.setContentText("Please enter a new name:");
-        Optional<String> result = dialog.showAndWait();
-        if (result.isPresent()) {
-            gl.getGame(selectedGT).addTable(result.get());
-            gl.saveGame(selectedGT);
-        }
+        if (hasSelectedGame()) {
+            TextInputDialog dialog = new TextInputDialog("New Table");
+            dialog.setTitle("New Table");
+            dialog.setHeaderText("Create a new Lookup Table");
+            dialog.setContentText("Please enter a new name:");
+            Optional<String> result = dialog.showAndWait();
+            if (result.isPresent()) {
+                this.getSelectedGame().addTable(result.get());
+                gl.saveGame(getSelectedGame());
+            }
+        } else {
+            this.noGameSelectedAlert();
+        }    
     }
     
     @FXML 
     public void handleTableDelete() {
-        String item = tableList.getSelectionModel().getSelectedItem();
-        System.out.println(item);
-        System.out.println(selectedTable);
-        if(item != null && item.equals(selectedTable)) {
-            Alert alert = new Alert(AlertType.CONFIRMATION);
-            alert.setTitle("Deleting " + item);
-            alert.setHeaderText("You are about to delete a Table");
-            alert.setContentText("Do you really want to delete " + item + "?");
+        if (hasSelectedGame()) {    
+            String item = tableList.getSelectionModel().getSelectedItem();
+            if(item != null && item.equals(selectedTable)) {
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Deleting " + item);
+                alert.setHeaderText("You are about to delete a Table");
+                alert.setContentText("Do you really want to delete " + item + "?");
 
-            Optional<ButtonType> result = alert.showAndWait();
-            if (result.get() == ButtonType.OK){
-                gl.getGame(selectedGT).removeTable(item);
-                gl.saveGame(selectedGT);
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK){
+                    this.getSelectedGame().removeTable(item);
+                    gl.saveGame(getSelectedGame());
+                }
             }
+        } else {
+            this.noGameSelectedAlert();
+        }    
+    }
+    
+    @FXML 
+    public void handleCharTypeSelection() {
+        String item = charTypeList.getSelectionModel().getSelectedItem();
+        if(item != null) {
+            this.selectedCT = item;
+            //this.loadCharType();
         }
+    }
+    
+    @FXML 
+    public void handleCharSelection() {
+        String item = charList.getSelectionModel().getSelectedItem();
+        if(item != null) {
+            this.selectedChar = item;
+            //this.loadCharType();
+        }
+    }
+    
+    @FXML 
+    public void handleCTAdd() {
+        if (hasSelectedGame()) {
+            TextInputDialog dialog = new TextInputDialog("New Character Type");
+            dialog.setTitle("New Character Type");
+            dialog.setHeaderText("Create a new Character Type");
+            dialog.setContentText("Please enter a new name:");
+            Optional<String> result = dialog.showAndWait();
+            if (result.isPresent()) {
+                CharacterType ct = new CharacterType();
+                ct.setName(result.get());
+                this.getSelectedGame().addCharacterType(ct);
+                gl.saveGame(getSelectedGame());
+            }
+        } else {
+            this.noGameSelectedAlert();
+        }
+    }
+    
+    @FXML 
+    public void handleCTDelete() {
+        if (hasSelectedGame()) {
+            String item = charTypeList.getSelectionModel().getSelectedItem();
+            if(item != null && item.equals(selectedCT)) {
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Deleting " + item);
+                alert.setHeaderText("You are about to delete a Character Type");
+                alert.setContentText("Do you really want to delete " + item + "?");
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK){
+                    this.getSelectedGame().removeTable(item);
+                    gl.saveGame(getSelectedGame());
+                }
+            }
+        } else {
+            this.noGameSelectedAlert();
+        }
+    }
+    
+    @FXML 
+    public void handleCharAdd() {
+        if (hasSelectedGame()) {
+            if(this.getSelectedGame().getCharacterTypes() != null && !this.getSelectedGame().getCharacterTypes().isEmpty()) {
+
+                Dialog dialog = new Dialog();
+                dialog.setTitle("New Character");
+                dialog.setHeaderText("Create a new Character");
+
+                ButtonType createButtonType = new ButtonType("Create", ButtonData.OK_DONE);
+                dialog.getDialogPane().getButtonTypes().addAll(createButtonType, ButtonType.CANCEL);
+
+                TextField charName = new TextField();
+                charName.setPromptText("Character Name");
+                ChoiceBox charType = new ChoiceBox(this.getSelectedGame().getCharTypeObsList());
+                charType.getSelectionModel().select(0);
+
+                GridPane grid = new GridPane();
+                grid.setHgap(10);
+                grid.setVgap(10);
+                grid.setPadding(new Insets(20, 150, 10, 10));
+
+                grid.add(new Label("Please enter a new name:"), 0, 0);
+                grid.add(charName, 1, 0);
+                grid.add(new Label("Select a Character Type:"), 0, 1);
+                grid.add(charType, 1, 1);
+
+                Node loginButton = dialog.getDialogPane().lookupButton(createButtonType);
+                loginButton.setDisable(true);
+
+                charName.textProperty().addListener((observable, oldValue, newValue) -> loginButton.setDisable(newValue.trim().isEmpty()));
+
+                dialog.getDialogPane().setContent(grid);
+
+                Optional result = dialog.showAndWait();
+
+                if (result.isPresent()) {
+                    Character c = new Character();
+                    c.setName(charName.getText());
+                    c.setGameType(getSelectedGame());
+                    c.setCharacterType(charType.getSelectionModel().getSelectedItem().toString());
+                    getSelectedGame().addCharacter(c);
+                    gl.saveGame(getSelectedGame());
+                }
+            } else {
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("New Character");
+                alert.setHeaderText("Please create a Character Type first");
+                alert.showAndWait();
+            }
+        } else {
+            this.noGameSelectedAlert();
+        }
+    }
+    
+    @FXML 
+    public void handleCharDelete() {
+        if (hasSelectedGame()) {
+            String item = charList.getSelectionModel().getSelectedItem();
+            if(item != null && item.equals(selectedChar)) {
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Deleting " + item);
+                alert.setHeaderText("You are about to delete a Character");
+                alert.setContentText("Do you really want to delete " + item + "?");
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK){
+                    this.getSelectedGame().removeCharacter(item);
+                    gl.saveGame(getSelectedGame());
+                }
+            }   
+        } else {
+            this.noGameSelectedAlert();
+        }        
+    }
+    
+    private GameType getSelectedGame() {
+        if(selectedGT != null) {
+            return gl.getGame(selectedGT);
+        }
+        System.out.println("GAME NOT FOUND: " + selectedGT);
+        return null;
+    }
+    
+    private boolean hasSelectedGame() {
+        return getSelectedGame() != null;
+    }
+    
+    private void noGameSelectedAlert() {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("No Context");
+        alert.setHeaderText("Please select or create a Game Type first");
+        alert.showAndWait();
     }
     
 }
